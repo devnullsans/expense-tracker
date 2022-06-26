ce932239c9f15a129ce9159d8f1d375d9a3f591a
Paul <dedsecdarknet@gmail.com>
2022-06-27 03:10:03 +0530

    Update _db.js

--- api/_db.js
+++ api/_db.js
@@ -4,14 +4,14 @@ const dbn = process.env.MONGODB_NAME;
 let conn = global.conn;
 export default async () => {
   if (conn) {
-    console.log("mongodb connection cached");
+    console.log("mongodb cache");
     return conn;
   }
   try {
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
     const db = await client.db(dbn);
-    console.log("mongodb connection fresh");
+    console.log("mongodb fresh");
     return global.conn = conn = db;
   } catch (err) {
     throw new Error(err);
