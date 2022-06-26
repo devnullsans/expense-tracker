72b3f03253b7a61de8d0dd9e879654ea13fe406e
Paul S <dedsecdarknet@gmail.com>
2022-06-27 00:05:49 +0530

    lots of chaings

--- api/index.js
+++ api/index.js
@@ -7,10 +7,38 @@ export default async (req, res) => {
     const collection = db.collection('expenses');
     const data = await collection.find({}).toArray();
     console.log('data', data);
-    console.log('req.props', Object.getOwnPropertyNames(req));
+    console.log('req.url', req.url);
+    console.log('req.method', req.method);
+    console.log('req.statusCode', req.statusCode);
+    console.log('req.statusMessage', req.statusMessage);
+    console.log('req.cookies', req.cookies);
+    console.log('req.body', req.body);
     res.status(200).json({ message: 'Okay' });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Internal Sever Error' });
   }
 }
+/*
+Connected successfully to mongodb
+data []
+req.props [
+  'socket',
+  'httpVersionMajor',
+  'httpVersionMinor',
+  'httpVersion',
+  'complete',
+  'rawHeaders',
+  'rawTrailers',
+  'aborted',
+  'upgrade',
+  'url',
+  'method',
+  'statusCode',
+  'statusMessage',
+  'client',
+  'cookies',
+  'query',
+  'body'
+]
+*/
