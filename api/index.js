da118ef590ef037df6c4038fd2db82d7339d347b
Paul S <dedsecdarknet@gmail.com>
2022-06-27 00:59:47 +0530

    GET-POST

--- api/index.js
+++ api/index.js
@@ -5,17 +5,25 @@ export default async (req, res) => {
   try {
     const db = await dbc();
     const collection = db.collection('expenses');
-    const data = await collection.find({}).toArray();
-    console.log('data', data);
     console.log('req.url', req.url);
     console.log('req.method', req.method);
     console.log('req.cookies', req.cookies);
     console.log('req.query', req.query);
     console.log('req.body', req.body);
-    res.status(200).json({ success: true, data });
+    switch (req.method) {
+      case 'GET': {
+        return res.status(200).json({ data: await collection.find({}).toArray() });
+      }
+      case 'POST': {
+				const { expense } = body;
+				return res.status(200).json({ data: await collection.insertOne(expense) });
+			}
+      default:
+        break;
+    }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ success: false, info: 'Internal Sever Error' });
+    res.status(500).json({ error: 'Internal Sever Error' });
   }
 }
 /*
