5830b772af53c133554172284bffce0ebb6d897e
Paul S <dedsecdarknet@gmail.com>
2022-06-26 22:29:24 +0530

    formatted

--- api/_utils/db.js
+++ api/_utils/db.js
@@ -3,12 +3,12 @@ const uri = process.env.MONGODB_URI;
 const dbn = process.env.MONGODB_NAME;
 conn = null;
 export default async () => {
-	if (conn) return conn;
-	try {
-		const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-		const db = await client.db(dbn);
-		return conn = db;
-	} catch (err) {
-		throw new Error(err);
-	}
+  if (conn) return conn;
+  try {
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = await client.db(dbn);
+    return conn = db;
+  } catch (err) {
+    throw new Error(err);
+  }
 }
--- api/index.js
+++ api/index.js
@@ -2,15 +2,15 @@ import db from './_utils/db';
 
 export default async (req, res) => {
 
-	try {
-		const dbc = await db();
-		const collection = dbc.collection('expenses');
+  try {
+    const dbc = await db();
+    const collection = dbc.collection('expenses');
     const data = await collection.find({}).toArray();
     console.log(data);
     console.log(req);
-		res.status(200).json({ message: 'Okay' });
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ message: 'Internal Sever Error' });
-	}
+    res.status(200).json({ message: 'Okay' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Sever Error' });
+  }
 }
