cbc9260e53963cffde14db38dbb908a77c75043c
Paul <dedsecdarknet@gmail.com>
2022-06-27 03:50:32 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import dbc from './_db';
+import { ObjectID } from 'mongodb';
 
 export default async (req, res) => {
 
@@ -15,11 +16,20 @@ export default async (req, res) => {
         return res.status(200).json({ data: await collection.find({}).toArray() });
       }
       case 'POST': {
-				const { expense } = req.body;
-				return res.status(200).json({ data: await collection.insertOne(expense) });
-			}
+        const { expense } = req.body;
+        return res.status(200).json({ data: await collection.insertOne(expense) });
+      }
+      case 'PUT': {
+        const { id } = req.query;
+        const { expense } = req.body;
+        return res.status(200).json({ data: await collection.findOneAndUpdate({ _id: id }, { $set: expense })});
+      }
+      case 'DELETE': {
+        const { id } = req.query;
+        return res.status(200).json({ data: await collection.findOneAndDelete({ _id: id });
+      }
       default:
-        break;
+        return res.status(400).json({ message: 'HTTP Method not supported' });
     }
   } catch (err) {
     console.error(err);
