ccebf7775e465054e9424526f3222a35e5af8664
Paul S <dedsecdarknet@gmail.com>
2022-06-27 00:42:42 +0530

    more db logs

--- api/_db.js
+++ api/_db.js
@@ -1,14 +1,13 @@
 import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI;
 const dbn = process.env.MONGODB_NAME;
-let num = global.num ?? 0;
 let conn = global.conn;
 export default async () => {
   console.log('num', num);
   if (conn) {
     console.log("mongodb connection cached");
     return conn;
-  } else global.num = num += 1
+  }
   try {
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
