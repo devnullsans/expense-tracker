20861e95ccb6667636a5f36691e6db291d8c8318
Paul S <dedsecdarknet@gmail.com>
2022-06-26 22:38:30 +0530

    conn update

--- api/_utils/db.js
+++ api/_utils/db.js
@@ -5,8 +5,10 @@ conn = null;
 export default async () => {
   if (conn) return conn;
   try {
-    const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
     const db = await client.db(dbn);
+    console.log("Connected successfully to mongodb");
     return conn = db;
   } catch (err) {
     throw new Error(err);
