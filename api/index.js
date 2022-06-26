4ca97fa83adc488c931a965422cfc805a838d81d
Paul S <dedsecdarknet@gmail.com>
2022-06-27 00:18:18 +0530

    final logs for req

--- api/index.js
+++ api/index.js
@@ -9,37 +9,21 @@ export default async (req, res) => {
     console.log('data', data);
     console.log('req.url', req.url);
     console.log('req.method', req.method);
-    console.log('req.statusCode', req.statusCode);
-    console.log('req.statusMessage', req.statusMessage);
     console.log('req.cookies', req.cookies);
     console.log('req.query', req.query);
     console.log('req.body', req.body);
-    res.status(200).json({ message: 'Okay' });
+    res.status(200).json({ success: true, data });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Sever Error' });
+    res.status(500).json({ success: false, info: 'Internal Sever Error' });
   }
 }
 /*
 Connected successfully to mongodb
 data []
-req.props [
-  'socket',
-  'httpVersionMajor',
-  'httpVersionMinor',
-  'httpVersion',
-  'complete',
-  'rawHeaders',
-  'rawTrailers',
-  'aborted',
-  'upgrade',
-  'url',
-  'method',
-  'statusCode',
-  'statusMessage',
-  'client',
-  'cookies',
-  'query',
-  'body'
-]
+req.url /api
+req.method GET
+req.cookies {}
+req.query [Object: null prototype] {}
+req.body undefined
 */
