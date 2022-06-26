11bb944399cb5d122b8b80d91d06a354fa3b9dbb
Paul S <dedsecdarknet@gmail.com>
2022-06-26 22:26:47 +0530

    mods for logs

--- api/_utils/db.js
+++ api/_utils/db.js
@@ -1,21 +1,14 @@
 import { MongoClient } from 'mongodb';
-
 const uri = process.env.MONGODB_URI;
 const dbn = process.env.MONGODB_NAME;
-
-let conn = null;
-
+conn = null;
 export default async () => {
-
 	if (conn) return conn;
-
 	try {
 		const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 		const db = await client.db(dbn);
-
-		conn = db;
-		return db;
-	} catch (e) {
-		throw new Error(e);
+		return conn = db;
+	} catch (err) {
+		throw new Error(err);
 	}
 }
--- api/index.js
+++ api/index.js
@@ -1,60 +1,16 @@
 import db from './_utils/db';
-import authenticate from './_utils/auth';
 
 export default async (req, res) => {
-	const { body } = req;
-	const { headers } = req;
-	const { authorization } = headers;
-
-	// First, check if the API request is authorized
-	if (!authorization || authorization.indexOf('Basic ') !== 0 || authenticate(authorization) === false)
-		return res.status(401).json({ message: 'Unauthorized' });
-
-
-	// Second, check if the collection name is present in the req.body
-	const { collectionName } = body;
-
-	if (!collectionName)
-		return res.status(400).json({ message: 'Please provide a valid collection name' })
-
-
-	/**
-	 * Now that we have everything that is needed,
-	 * let's do the heavy lifting to get the database connection instance
-	 * and return the desired result.
-	*/
 
 	try {
-		const databaseConnection = await db();
-		const collection = databaseConnection.collection(collectionName);
-
-		// Conduct the database operation based on the HTTP method
-		switch (req.method) {
-			case 'GET': {
-				return res.status(200).json({ data: await collection.find({}).toArray() });
-			}
-
-			case 'PUT': {
-				const { filter, document } = body;
-				return res.status(200).json({ data: await collection.updateOne(filter, { $set: document }, { upsert: true }) });
-			}
-
-			case 'POST': {
-				const { document } = body;
-				return res.status(200).json({ data: await collection.insertOne(document) });
-			}
-
-			case 'DELETE': {
-				const { filter } = body;
-				return res.status(200).json({ data: await collection.deleteOne(filter) });
-			}
-
-			default:
-				return res.status(400).json({ message: 'HTTP Method not supported' });
-		}
-
-	} catch (e) {
-		console.error(e);
-		res.status(500).json({ message: 'Internal Sever Error' })
+		const dbc = await db();
+		const collection = dbc.collection('expenses');
+    const data = await collection.find({}).toArray();
+    console.log(data);
+    console.log(req);
+		res.status(200).json({ message: 'Okay' });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: 'Internal Sever Error' });
 	}
 }
