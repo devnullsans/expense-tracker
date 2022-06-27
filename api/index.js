709743b3a424a27ba5fb738f3af69a99c250bdd9
DevNullSans <dedsecdarknet@gmail.com>
2022-06-27 15:34:49 +0530

    chains

--- api/index.js
+++ api/index.js
@@ -3,39 +3,39 @@ import { ObjectId } from 'mongodb';
 
 export default async (req, res) => {
 
-	try {
-		const db = await dbc();
-		const collection = db.collection('expenses');
-		console.log('req.url', req.url);
-		console.log('req.method', req.method);
-		console.log('req.cookies', req.cookies);
-		console.log('req.query', req.query);
-		console.log('req.body', req.body);
-		switch (req.method) {
-			case 'GET': {
-				const { to, fo } = req.query;
-				return res.status(200).json({ data: await collection.find({ timestamp: { $gt: Number(fo), $lt: Number(to) } }).sort({ timestamp: 1 }) });
-			}
-			case 'POST': {
-				const { expense } = req.body;
-				return res.status(200).json({ data: await collection.insertOne(expense) });
-			}
-			case 'PUT': {
-				const { id } = req.query;
-				const { expense } = req.body;
-				return res.status(200).json({ data: await collection.findOneAndUpdate({ _id: ObjectId(id) }, { $set: expense }) });
-			}
-			case 'DELETE': {
-				const { id } = req.query;
-				return res.status(200).json({ data: await collection.findOneAndDelete({ _id: ObjectId(id) }) });
-			}
-			default:
-				return res.status(400).json({ message: 'HTTP Method not supported' });
-		}
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ error: 'Internal Sever Error' });
-	}
+  try {
+    const db = await dbc();
+    const collection = db.collection('expenses');
+    console.log('req.url', req.url);
+    console.log('req.method', req.method);
+    console.log('req.cookies', req.cookies);
+    console.log('req.query', req.query);
+    console.log('req.body', req.body);
+    switch (req.method) {
+      case 'GET': {
+        const { to, fo } = req.query;
+        return res.status(200).json({ data: await collection.find({ timestamp: { $gt: Number(fo), $lt: Number(to) } }).toArray() });
+      }
+      case 'POST': {
+        const { expense } = req.body;
+        return res.status(200).json({ data: await collection.insertOne(expense) });
+      }
+      case 'PUT': {
+        const { id } = req.query;
+        const { expense } = req.body;
+        return res.status(200).json({ data: await collection.findOneAndUpdate({ _id: ObjectId(id) }, { $set: expense }) });
+      }
+      case 'DELETE': {
+        const { id } = req.query;
+        return res.status(200).json({ data: await collection.findOneAndDelete({ _id: ObjectId(id) }) });
+      }
+      default:
+        return res.status(400).json({ message: 'HTTP Method not supported' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Sever Error' });
+  }
 }
 /*
 Connected successfully to mongodb
