63f03a2505b22c06e34c47b26ac620bf6698c96e
Paul S <dedsecdarknet@gmail.com>
2022-07-02 00:05:28 +0530

    hacky API call

---
+++ jnks/thots/api.js
@@ -0,0 +1,10 @@
+// expense stats
+// /api/expence/v0?id
+// growing back expense list
+// /api/expence/v0?to&fo
+// expense save
+// /api/expence/v0
+// expense update
+// /api/expence/v0
+// expense delete
+// /api/expence/v0
